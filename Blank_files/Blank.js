032eea762ec8b01967cb8800f4fd7057739d2342
overstable <muhammadrezay@gmail.com>
2014-02-07 23:04:31 +0100

    blank page - test

---
+++ Blank_files/Blank.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20140207
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Blank_files/BlankMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
