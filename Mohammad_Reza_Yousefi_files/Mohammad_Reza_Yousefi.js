fb79e8138bc42781471cfefe8280a522d4289d28
overstable <muhammadrezay@gmail.com>
2014-02-07 22:39:15 +0100

    added mohammad reza page
    just a second page for mohammad reza

---
+++ Mohammad_Reza_Yousefi_files/Mohammad_Reza_Yousefi.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20140207
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Mohammad_Reza_Yousefi_files/Mohammad_Reza_YousefiMoz.css')
+Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
