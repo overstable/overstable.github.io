94bf891bcf9bc73e445d0b60730753297734301f
overstable <muhammadrezay@gmail.com>
2014-02-09 15:27:06 +0100

    clean up

--- Welcome_files/Welcome.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140209
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('Welcome_files/WelcomeMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id2');applyEffects()}
