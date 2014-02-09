d63960783ba7f3c464dd948ae2039dad821b1bbe
overstable <muhammadrezay@gmail.com>
2014-02-09 14:24:25 +0100

    reza removed

--- reza/reza-home_files/reza-home.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20140209
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('reza-home_files/reza-homeMoz.css')
-fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id1');applyEffects()}
